cc9ec06cf0ecbc7b0644aaa71680741c83c09907
notoj <1358519169@qq.com>
2020-01-15 17:49:22 +0800

    draw player2 key tips

--- screens/level/pvpScreen.lua
+++ screens/level/pvpScreen.lua
@@ -7,10 +7,24 @@ function Screen:activate()
     self:addFighterList(Player, base.guiWidth/2, base.guiHeight/4*3, math.pi/2*3, base.cPlayer2)
     self:addFighterList(Player, base.guiWidth/2, base.guiHeight/4, math.pi/2, base.cPlayer1, keys.L, keys.R)
 
-    
+    --- canvas
+    self.canvas = love.graphics.newCanvas()
+    love.graphics.setCanvas(self.canvas)
+        love.graphics.clear()
+        love.graphics.setColor(base.cWhite)
+        base.print('L', base.guiBorder, base.guiHeight-base.guiBorder, 'left', 'bottom')
+        base.print('R', base.guiWidth-base.guiBorder, base.guiHeight-base.guiBorder, 'right', 'bottom')
+    love.graphics.setCanvas()
+    ---
 end
 
 
+function Screen:draw()
+    Screen.super.draw(self)
+
+    love.graphics.setColor(base.cWhite)
+    love.graphics.draw(self.canvas, base.guiWidth, base.guiHeight, math.pi, 1, 1)
+end
 
 
 return Screen
